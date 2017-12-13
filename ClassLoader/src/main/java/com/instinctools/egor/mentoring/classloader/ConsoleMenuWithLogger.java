315ae7934ee029cdb0708248d10de1861f20bdbf
Unknown <egid73@mail.ru>
2017-12-13 19:06:44 +0300

    fresh commit

---
+++ ClassLoader/src/main/java/com/instinctools/egor/mentoring/classloader/ConsoleMenuWithLogger.java
@@ -0,0 +1,49 @@
+package com.instinctools.egor.mentoring.classloader;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Scanner;
+
+public class ConsoleMenuWithLogger {
+
+    Class<?> aClass = null;
+    Logger log = LogManager.getLogger(ConsoleMenuWithLogger.class);
+    public ConsoleMenuWithLogger() {
+        while (true){
+            log("Menu: \n1-start \n0-exit \n");
+            Scanner scanner = new Scanner(System.in);
+            Integer chose = scanner.nextInt();
+            if(chose == 1) {
+                log("Input full path to jar: ");
+                String path = scanner.next();
+                log("Input jar name: ");
+                String jarName = scanner.next();
+                log("Input full Class name: ");
+                String className = scanner.next();
+                log("Input full Class name: ");
+                String methodName = scanner.next();
+                try {
+                   MyLoader loader = MyLoader.getInstance(path, jarName);
+                   Class<?> aClass = loader.loadClass(className);
+                   log("Here some Message from class: \n");
+                   loader.getMessageFromClass(aClass, methodName);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    private void log(String s){
+        log.info(s);
+    }
+}
---
+++ ClassLoader/src/main/java/com/instinctools/egor/mentoring/classloader/Main.java
@@ -0,0 +1,8 @@
+package com.instinctools.egor.mentoring.classloader;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ConsoleMenuWithLogger helper = new ConsoleMenuWithLogger();
+    }
+}
