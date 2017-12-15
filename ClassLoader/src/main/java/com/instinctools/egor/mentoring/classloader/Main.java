4fcb774b2d66f288767636393bd4873e3ac5427e
ShuFFel <egid73@mail.ru>
2017-12-15 12:59:39 +0300

    Update Main.java

--- ClassLoader/src/main/java/com/instinctools/egor/mentoring/classloader/Main.java
+++ ClassLoader/src/main/java/com/instinctools/egor/mentoring/classloader/Main.java
@@ -3,6 +3,6 @@ package com.instinctools.egor.mentoring.classloader;
 public class Main {
 
     public static void main(String[] args) {
-        ConsoleMenuWithLogger helper = new ConsoleMenuWithLogger(); //here i use DRY principle 
+        ConsoleMenuWithLogger helper = new ConsoleMenuWithLogger(); //here i use KISS principle 
     }
 }
