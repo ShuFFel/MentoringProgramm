385a77902f59fd6b1b913503952308351356c4ea
Unknown <egid73@mail.ru>
2017-12-22 19:50:11 +0300

    Delete AccountService

--- JPATask/src/main/java/com/instinctools/egor/mentoring/JPA/Service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package com.instinctools.egor.mentoring.JPA.Service;
-
-import com.instinctools.egor.mentoring.JPA.Entity.Account;
-
-public interface AccountService {
-    public void createAccount(Account account);
-    public void deleteAccount(Account account);
-    public void updateAccount(Account account);
-}
