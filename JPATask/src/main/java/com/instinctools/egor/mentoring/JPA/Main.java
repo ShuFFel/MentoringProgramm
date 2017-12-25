afa35697d91c6e79c36af92c555108f5f118ffe4
ShuFFel <egid73@mail.ru>
2017-12-25 13:48:53 +0300

    Update Main.java

--- JPATask/src/main/java/com/instinctools/egor/mentoring/JPA/Main.java
+++ JPATask/src/main/java/com/instinctools/egor/mentoring/JPA/Main.java
@@ -1,11 +1,12 @@
 package com.instinctools.egor.mentoring.JPA;
 
 import com.instinctools.egor.mentoring.JPA.CommandLineMenu.ClientWorkingMenu;
+import com.instinctools.egor.mentoring.JPA.Service.DAOServiceImpl.ClientServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        ClientWorkingMenu menu =new ClientWorkingMenu();
+        ClientWorkingMenu menu =new ClientWorkingMenu(new ClientServiceImpl());
         menu.start();
     }
 
